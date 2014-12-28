dada606351d00e0f44d04524cc717819ca9bedfd
BuildTools <unconfigured@null.spigotmc.org>
2014-12-28 03:11:46 +0000

    Initial commit

---
+++ src/main/java/com/grim/spnrpg/Main.java
@@ -0,0 +1,16 @@
+package com.grim.spnrpg;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin{
+
+    @Override
+    public void onDisable() {
+
+    }
+
+    @Override
+    public void onEnable() {
+
+    }
+}
